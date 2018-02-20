a7ab2ce75aba00fcc212b3be8e01e0e1425d8a7b
alienus <bradleywitcher@gmail.com>
2018-02-19 23:08:53 -0500

    commit

--- owtf/plugins/web/external/Infrastructure_Configuration_Management@OWTF-CM-003.py
+++
@@ -1,9 +0,0 @@
-from owtf.plugin.plugin_helper import plugin_helper
-
-
-DESCRIPTION = "Plugin to assist manual testing"
-
-
-def run(PluginInfo):
-    Content = plugin_helper.HtmlString("Intended to show helpful info in the future")
-    return Content
--- owtf/plugins/web/external/User_Input_as_a_Loop_Counter@OWTF-DS-005.py
+++
@@ -1,9 +0,0 @@
-from owtf.plugin.plugin_helper import plugin_helper
-
-
-DESCRIPTION = "Plugin to assist manual testing"
-
-
-def run(PluginInfoz):
-    Content = plugin_helper.HtmlString("Intended to show helpful info in the future")
-    return Content
